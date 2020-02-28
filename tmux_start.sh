ce40881459ce3a5c076bb86a1e91dde28569fae7
David Torrens <61469929+DT-was-an-ET@users.noreply.github.com>
2020-02-28 13:56:43 +0000

    increased on threshold to 56

--- tmux_start.sh
+++ tmux_start.sh
@@ -3,4 +3,4 @@ cd /home/pi/fanshim
 echo looking to kill any existing fanshim session
 tmux kill-session -t fanshim
 echo now new tmux fanshim session
-tmux new-session -d -s fanshim 'python3 examples/automatic.py --off-threshold 54 --on-threshold 55 --verbose'
+tmux new-session -d -s fanshim 'python3 examples/automatic.py --off-threshold 54 --on-threshold 56 --verbose'
