7b393fe2ff08609e1e48af3ad53715715306895f
David Torrens <61469929+DT-was-an-ET@users.noreply.github.com>
2020-03-11 12:35:02 +0000

    Simplified Switching

--- examples/automatic.py
+++ examples/automatic.py
@@ -26,62 +26,62 @@ args = parser.parse_args()
 
 
 def clean_exit(signum, frame):
-    set_fan(False)
-    if not args.noled:
-        fanshim.set_light(0, 0, 0)
-    sys.exit(0)
+	set_fan(False)
+	if not args.noled:
+		fanshim.set_light(0, 0, 0)
+	sys.exit(0)
 
 
 def update_led_temperature(temp):
-    led_busy.acquire()
-    temp = float(temp)
-    temp -= args.off_threshold
-    temp /= float(args.on_threshold - args.off_threshold)
-    temp = max(0, min(1, temp))
-    temp = 1.0 - temp
-    temp *= 120.0
-    temp /= 360.0
-    r, g, b = [int(c * 255.0) for c in colorsys.hsv_to_rgb(temp, 1.0, args.brightness / 255.0)]
-    fanshim.set_light(r, g, b)
-    led_busy.release()
+	led_busy.acquire()
+	temp = float(temp)
+	temp -= args.off_threshold
+	temp /= float(args.on_threshold - args.off_threshold)
+	temp = max(0, min(1, temp))
+	temp = 1.0 - temp
+	temp *= 120.0
+	temp /= 360.0
+	r, g, b = [int(c * 255.0) for c in colorsys.hsv_to_rgb(temp, 1.0, args.brightness / 255.0)]
+	fanshim.set_light(r, g, b)
+	led_busy.release()
 
 
 def get_cpu_temp():
-    t = psutil.sensors_temperatures()
-    for x in ['cpu-thermal', 'cpu_thermal']:
-        if x in t:
-            return t[x][0].current
-    print("Warning: Unable to get CPU temperature!")
-    return 0
+	t = psutil.sensors_temperatures()
+	for x in ['cpu-thermal', 'cpu_thermal']:
+		if x in t:
+			return t[x][0].current
+	print("Warning: Unable to get CPU temperature!")
+	return 0
 
 
 def get_cpu_freq():
-    freq = psutil.cpu_freq()
-    return freq
+	freq = psutil.cpu_freq()
+	return freq
 
 
 def set_fan(status):
-    global enabled
-    changed = False
-    if status != enabled:
-        changed = True
-        fanshim.set_fan(status)
-    enabled = status
-    return changed
+	global enabled
+	changed = False
+	if status != enabled:
+		changed = True
+		fanshim.set_fan(status)
+	enabled = status
+	return changed
 
 
 def set_automatic(status):
-    global armed, last_change
-    armed = status
-    last_change = 0
+	global armed, last_change
+	armed = status
+	last_change = 0
 
 
 if args.threshold > -1 or args.hysteresis > -1:
-    print("""
+	print("""
 The --threshold and --hysteresis parameters have been deprecated.
 Use --on-threshold and --off-threshold instead!
 """)
-    sys.exit(1)
+	sys.exit(1)
 
 
 fanshim = FanShim()
@@ -96,62 +96,64 @@ last_change = 0
 signal.signal(signal.SIGTERM, clean_exit)
 
 if args.noled:
-    led_busy.acquire()
-    fanshim.set_light(0, 0, 0)
-    led_busy.release()
+	led_busy.acquire()
+	fanshim.set_light(0, 0, 0)
+	led_busy.release()
 
 t = get_cpu_temp()
 if t >= args.threshold:
-    last_change = get_cpu_temp()
-    set_fan(True)
+	last_change = get_cpu_temp()
+	set_fan(True)
 
 
 if not args.nobutton:
-    @fanshim.on_release()
-    def release_handler(was_held):
-        global armed
-        if was_held:
-            set_automatic(not armed)
-        elif not armed:
-            set_fan(not enabled)
+	@fanshim.on_release()
+	def release_handler(was_held):
+		global armed
+		if was_held:
+			set_automatic(not armed)
+		elif not armed:
+			set_fan(not enabled)
 
-    @fanshim.on_hold()
-    def held_handler():
-        global led_busy
-        if args.noled:
-            return
-        led_busy.acquire()
-        for _ in range(3):
-            fanshim.set_light(0, 0, 255)
-            time.sleep(0.04)
-            fanshim.set_light(0, 0, 0)
-            time.sleep(0.04)
-        led_busy.release()
+	@fanshim.on_hold()
+	def held_handler():
+		global led_busy
+		if args.noled:
+			return
+		led_busy.acquire()
+		for _ in range(3):
+			fanshim.set_light(0, 0, 255)
+			time.sleep(0.04)
+			fanshim.set_light(0, 0, 0)
+			time.sleep(0.04)
+		led_busy.release()
 
 
 try:
-    while True:
-        t = get_cpu_temp()
-        f = get_cpu_freq()
-        was_fast = is_fast
-        is_fast = (int(f.current) == int(f.max))
-        if args.verbose:
-            print("Current: {:05.02f} Target: {:05.02f} Freq {: 5.02f} Automatic: {} On: {}".format(t, args.off_threshold, f.current / 1000.0, armed, enabled))
+	while True:
+		t = get_cpu_temp()
+		#f = get_cpu_freq()
+		#was_fast = is_fast
+		#is_fast = (int(f.current) == int(f.max))
+		msg = "no_change"
 
-        if args.preempt and is_fast and was_fast:
-            enable = True
-        elif armed:
-            if t >= args.on_threshold:
-                enable = True
-            elif t <= args.off_threshold:
-                enable = False
+		#if args.preempt and is_fast and was_fast:
+		#	enable = True
+		#elif armed:
+		if t >= args.on_threshold:
+			enable = True
+			msg = "switched_on"
+		elif t <= args.off_threshold:
+			enable = False
+			msg =  "switched off"
 
-        if set_fan(enable):
-            last_change = t
+		if set_fan(enable):
+			last_change = t
 
-        if not args.noled:
-            update_led_temperature(t)
-
-        time.sleep(args.delay)
+		if not args.noled:
+			update_led_temperature(t)
+		if args.verbose:
+			print(msg,"Current: {:05.02f} 0n: {:05.02f} Off {: 5.02f}".format(t, args.on_threshold, args.off_threshold))
+		time.sleep(args.delay)
 except KeyboardInterrupt:
-    pass
+	pass
